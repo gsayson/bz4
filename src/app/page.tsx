6b4a37d93e0806eb7abbbb8fd729389866a60e6d
Gerard Sayson <geruls@broskiclan.org>
2024-07-01 20:14:25 +0800

    update project list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,14 @@ export default function Home() {
             <h1 className="text-3xl font-semibold mb-2 lg:mb-4">👋 hi, I&apos;m gerard</h1>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <a href={"https://www.codewars.com/users/Geruls"}><img src={"https://www.codewars.com/users/Geruls/badges/small"} alt={"my badge"} className={"mb-4 lg:mb-6"}/></a>
+            <p>(hi HCI pls admit me :D)</p>
             <p>
                 I&apos;m a full-stack developer who likes to dabble in everything, from web development with <a href="https://react.dev" className="link">React</a> and
                 {" "}<a href="https://www.solidjs.com" className="link">SolidJS</a> to compilers and
                 OS development. In addition, I also work with <a href="https://www.oracle.com/java/" className="link">Java</a> and
                 {" "}<a href="https://www.rust-lang.org" className="link">Rust</a>.
             </p>
-            <h2 className="text-2xl font-semibold mb-4 mt-6">🖥️ my cs journey</h2>
+            <h2 className="text-2xl font-semibold mb-4 mt-6">computer science</h2>
             <div className="space-y-4">
                 <p>
                     I started programming when I was 8 or 9 years old, beginning with the language of <a
@@ -33,15 +34,8 @@ export default function Home() {
                     SolidJS (in <a href="https://www.typescriptlang.org" className="link">TypeScript</a>).
                 </p>
             </div>
-            <h2 className="text-2xl font-semibold mb-4 mt-8">🤓 my mathematical hobby</h2>
+            <h2 className="text-2xl font-semibold mb-4 mt-8">mathematics</h2>
             <div className="space-y-4">
-                <p>
-                    I started gaining an interest in mathematics at the start of 2023, and
-                    what really pulled me in was the intricacy of mathematics. It all started
-                    when I watched <a href="https://www.youtube.com/playlist?list=PLwV-9DG53NDxU337smpTwm6sef4x-SCLv" className="link">this YouTube playlist
-                    on group theory</a>; that was the first time that I was ever fascinated by the allure of mathematics&apos; rigour and complexity. Mathematics
-                    was not just about playing with numbers anymore to me. It became an art &mdash; the art of proving.
-                </p>
                 <p>
                     Right now, I study <em>group theory</em> in my free time. <a href="https://en.wikipedia.org/wiki/Group_theory" className="link">Group theory</a>{" "}
                     is a part of the discipline of <a href="https://en.wikipedia.org/wiki/Abstract_algebra" className="link">abstract algebra</a>,
@@ -49,18 +43,23 @@ export default function Home() {
                     Group theory arises in many areas, for example cryptography, and puzzles, most notably <a href="https://en.wikipedia.org/wiki/Rubik%27s_Cube_group" className="link">the Rubik&apos;s cube</a>.
                 </p>
                 <p>
-                    Other than that, I&apos;m reading Axler&apos;s <em>Linear Algebra Done Right</em> to establish a foothold for myself in linear algebra.
+                    I&apos;m reading Axler&apos;s <em>Linear Algebra Done Right</em> to establish a foothold for myself in linear algebra, along with
+                    Casella &amp; Berger&apos;s <em>Statistical Inference</em> for statistics.
                 </p>
                 <p>
                     If you&apos;re interested in my mathematical publications which discuss higher-level mathematics, visit <Link href={"/publications"} className={"link"}>this page.</Link>
+                    Alternatively, feel free to check out my YouTube channel in the footer!
                 </p>
             </div>
             <h1 className="text-2xl font-semibold mb-4 mt-8">my projects</h1>
             <Card title="gsn.bz" desc="The very website you're looking at right now." url="https://gsn.bz"
                 github="https://github.com/gsayson/bz4"/>
+            <Card title="Gateway" desc="An authentication server running on MongoDB." github="https://github.com/gsayson/gateway"/>
+            <Card title="Project Gratitude" desc="A web app for students and teachers to express their gratitude to each other." github="https://github.com/gsayson/project-gratitude"/>
             <Card title="goGSN" desc="A link shortener running on Redis." github="https://github.com/gsayson/go-bz"/>
-            <Card title="Djinn" desc="A build-time-oriented Java framework with minimal runtime reflection."
+            <Card title="Djinn" desc="A build-time-oriented Java framework with minimal runtime reflection, written for a 2023 coding jam."
                 github="https://github.com/gsayson/djinn"/>
+            <Card title="goGSN" desc="A web server framework for Java, written for a 2022 coding jam." github="https://github.com/gsayson/go-bz"/>
             <h1 className="text-2xl font-semibold mb-4 mt-6">connect with me</h1>
             <p>
                 I&apos;m particularly active on the <a className="italic link" href="https://discordjug.net">Java
--- src/app/publications/page.tsx
+++ src/app/publications/page.tsx
@@ -1,36 +1,75 @@
 export default function Publications() {
     return (
-        <div>
-            <h1 className="text-3xl font-semibold mb-4 lg:mb-6">my publications</h1>
-            <p>
-                Here&apos;s a list of my publications in mathematics. I use
-                <a href={"https://typst.app"}>Typst</a> to create them.
-            </p>
-            <h2 className="text-2xl font-semibold mb-4 mt-6">further math 2023 notes</h2>
-            <div className="space-y-4">
-                <p>
-                    I wrote this last year in end-of-year 2023 as a pastime, with the aim
-                    of making a Further Mathematics textbook for fun. <em>Fortunately</em>,
-                    Cambridge just had to change the syllabus:
-                    <ul className={"list-disc ml-4 my-2"}>
-                        <li>remove mathematical induction conic sections</li>
-                        <li>use computers in applying Simpson&apos;s Rule, the trapezium rule, the Euler method and the improved Euler method (Heun&apos;s method)</li>
-                        <li>add multivariable calculus, but without the Jacobian</li>
-                    </ul>
-                </p>
-                <p>
-                    These notes are <em>very incomplete</em>, as I haven&apos;t had time to work on them in this year.
-                    The chapters included are:
-                    <ul className={"list-disc ml-4 my-2"}>
-                        <li>mathematical induction</li>
-                        <li>ordinary differential equations (incomplete)</li>
-                        <li>numerical methods of ordinary differential equations and integrals</li>
-                    </ul>
-                </p>
-                <p>
-                    You can access it <a href={"https://drive.google.com/file/d/1jYGmfsg_7SJtFY6pfKbHGm_AD21KWomj/view?usp=sharing"} className={"link"}>here</a>.
-                </p>
-            </div>
+      <div>
+          <h1 className="text-3xl font-semibold mb-4 lg:mb-6">my publications</h1>
+          <p>
+              I like to write about topics I find interesting; it also helps me learn as I write these for you.
+              Here&apos;s a list of my publications in mathematics. I use{" "}
+              <a href={"https://typst.app"} className="link">Typst</a> and <a href={"https://www.latex-project.org/"}
+                                                                              className="link">LaTeX</a> to create them.
+          </p>
+          <h2 className="text-2xl font-semibold mb-4 mt-12">further math 2023 notes</h2>
+        <div className="space-y-4">
+          <p>
+            I wrote this last year in end-of-year 2023 as a pastime, with the aim
+            of making a Further Mathematics textbook for fun. <em>Fortunately</em>,
+            Cambridge just had to change the syllabus:
+          </p>
+          <ul className={"list-disc ml-4 my-2"}>
+            <li>remove mathematical induction conic sections</li>
+            <li>use computers in applying Simpson&apos;s Rule, the trapezium rule, the Euler method and the
+              improved Euler method (Heun&apos;s method)
+            </li>
+            <li>add multivariable calculus, but without the Jacobian</li>
+          </ul>
+          <p>
+            These notes are <em>very incomplete</em>, as I haven&apos;t had time to work on them in this year.
+            The chapters included are:
+          </p>
+          <ul className={"list-disc ml-4 my-2"}>
+            <li>mathematical induction</li>
+            <li>ordinary differential equations (incomplete)</li>
+            <li>numerical methods of ordinary differential equations and integrals</li>
+          </ul>
+          <p>
+            You can access it <a
+            href={"https://drive.google.com/file/d/1jYGmfsg_7SJtFY6pfKbHGm_AD21KWomj/view?usp=sharing"}
+            className={"link"}>here</a>.
+          </p>
         </div>
+        <h2 className="text-2xl font-semibold mb-4 mt-12">h3 math 2025 notes</h2>
+        <div className="space-y-4">
+          <p>
+            I wrote this during the holidays, with the aim
+            of making a H3 Mathematics textbook for fun. This is intended
+            as a supplementary text, which I will try to complete after I sit my exams.
+          </p>
+          <p>
+          This is <em>very incomplete</em>, as I haven&apos;t had time to work on this.
+            The chapters included are:
+          </p>
+          <ul className={"list-disc ml-4 my-2"}>
+            <li>mathematical language (theorems & logic)</li>
+            <li>introduction to limits (discussing squeeze theorem, diff. from first principles, etc.)</li>
+          </ul>
+          <p>
+            You can access it <a
+            href={"https://github.com/gsayson/h3-mathematics-2025"}
+            className={"link"}>here</a>.
+          </p>
+        </div>
+        <h2 className="text-2xl font-semibold mb-4 mt-12">h2 further math 2025: mv calc</h2>
+        <div className="space-y-4">
+          <p>
+            I wrote this early this year to try to collect my knowledge of the multivariable calculus examined in 2025
+                  for Further Mathematics.
+              </p>
+              <p>
+                  You can access it <a
+                href={"https://drive.google.com/file/d/1Bt4dB2eTJhH8wXg5zdW1_Go3vSlWoP1p/view?usp=sharing"}
+                className={"link"}>here</a>.
+              </p>
+          </div>
+      </div>
     )
 }
